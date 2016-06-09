5787337ec00e8173cadabd24c25e591a6ae476e8
forbesjo <joseph.a.forbes@gmail.com>
2016-06-09 22:57:46 +0000

    Set up to use shaka player

--- src/js/videojs-dash.js
+++ src/js/videojs-dash.js
@@ -1,6 +1,6 @@
 import window from 'global/window';
 import videojs from 'video.js';
-import dashjs from 'dashjs';
+import shaka from 'shaka-player';
 
 let
   isArray = function(a) {
@@ -10,13 +10,10 @@ let
 /**
  * videojs-contrib-dash
  *
- * Use Dash.js to playback DASH content inside of Video.js via a SourceHandler
+ * Use the Shaka Player to playback DASH content inside of Video.js via a SourceHandler
  */
-class Html5DashJS {
+class Html5Shaka {
   constructor(source, tech) {
-    let options = tech.options_;
-    let player = videojs(options.playerId);
-
     this.tech_ = tech;
     this.el_ = tech.el();
     this.elParent_ = this.el_.parentNode;
@@ -26,59 +23,48 @@ class Html5DashJS {
       return;
     }
 
-    // While the manifest is loading and Dash.js has not finished initializing
+    shaka.polyfill.installAll();
+    
+    // While the manifest is loading and Shaka player has not finished initializing
     // we must defer events and functions calls with isReady_ and then `triggerReady`
     // again later once everything is setup
-    tech.isReady_ = false;
+    // tech.isReady_ = false;
 
-    if (Html5DashJS.updateSourceData) {
-      source = Html5DashJS.updateSourceData(source);
+    if (Html5Shaka.updateSourceData) {
+      source = Html5Shaka.updateSourceData(source);
     }
 
     let manifestSource = source.src;
-    this.keySystemOptions_ = Html5DashJS.buildDashJSProtData(source.keySystemOptions);
-
-    // Save the context after the first initialization for subsequent instances
-    Html5DashJS.context_ = Html5DashJS.context_ || {};
-
-    // reuse MediaPlayer if it already exists
-    if (!this.mediaPlayer_) {
-      this.mediaPlayer_ = dashjs.MediaPlayer(Html5DashJS.context_).create();
+    this.keySystemOptions_ = Html5Shaka.buildShakaProtData(source.keySystemOptions);
+    
+    
+    // reuse shakaPlayer if it already exists
+    if (!this.shakaPlayer_) {
+      this.shakaPlayer_ = new shaka.Player(this.el_);
     }
 
-    // Log MedaPlayer messages through video.js
-    if (Html5DashJS.useVideoJSDebug) {
-      videojs.log.warn('useVideoJSDebug has been deprecated.' +
-        ' Please switch to using beforeInitialize.');
-      Html5DashJS.useVideoJSDebug(this.mediaPlayer_);
-    }
-
-    if (Html5DashJS.beforeInitialize) {
-      Html5DashJS.beforeInitialize(player, this.mediaPlayer_);
-    }
-
-    // Must run controller before these two lines or else there is no
-    // element to bind to.
-    this.mediaPlayer_.initialize();
-    this.mediaPlayer_.attachView(this.el_);
-
-    // Dash.js autoplays by default, video.js will handle autoplay
-    this.mediaPlayer_.setAutoPlay(false);
-
+    this.shakaPlayer_.resetConfiguration();
+      
     // Attach the source with any protection data
-    this.mediaPlayer_.setProtectionData(this.keySystemOptions_);
-    this.mediaPlayer_.attachSource(manifestSource);
-
-    this.tech_.triggerReady();
+    if (this.keySystemOptions_) {
+      this.shakaPlayer_.configure({ drm:{ servers: this.keySystemOptions_ } });
+    }
+      
+    // var self = this;
+    this.shakaPlayer_.load(manifestSource).then(function() {
+      // self.tech_.triggerReady();
+    }, function(error) {
+      videojs.log.debug('error code ' + error.code);
+    });
   }
 
   /*
    * Iterate over the `keySystemOptions` array and convert each object into
-   * the type of object Dash.js expects in the `protData` argument.
+   * the type of object Shaka Player expects in the `protData` argument.
    *
    * Also rename 'licenseUrl' property in the options to an 'serverURL' property
    */
-  static buildDashJSProtData(keySystemOptions) {
+  static buildShakaProtData(keySystemOptions) {
     let output = {};
 
     if (!keySystemOptions || !isArray(keySystemOptions)) {
@@ -94,15 +80,16 @@ class Html5DashJS {
         delete options.licenseUrl;
       }
 
-      output[keySystem.name] = options;
+      output[keySystem.name] = options.serverURL;
     }
 
     return output;
   }
 
   dispose() {
-    if (this.mediaPlayer_) {
-      this.mediaPlayer_.reset();
+    if (this.shakaPlayer_) {
+      this.shakaPlayer_.unload();
+      this.shakaPlayer_.resetConfiguration();
     }
   }
 }
@@ -122,7 +109,7 @@ videojs.DashSourceHandler = function() {
     },
 
     handleSource: function(source, tech) {
-      return new Html5DashJS(source, tech);
+      return new Html5Shaka(source, tech);
     },
 
     canPlayType: function(type) {
@@ -145,5 +132,5 @@ if (!!window.MediaSource) {
   videojs.getComponent('Html5').registerSourceHandler(videojs.DashSourceHandler(), 0);
 }
 
-videojs.Html5DashJS = Html5DashJS;
-export default Html5DashJS;
+videojs.Html5Shaka = Html5Shaka;
+export default Html5Shaka;
